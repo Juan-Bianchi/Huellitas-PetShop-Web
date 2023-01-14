fadbac8e0f36c4a369b7d51d52442bc64af88b86
Juan-Bianchi <juanbianchi@icloud.com>
2023-01-14 17:19:20 -0300

    agrego lista de filtro para checks

--- assets/scripts/productos.js
+++ assets/scripts/productos.js
@@ -3,7 +3,10 @@ const{ createApp } = Vue
 createApp({
     data(){
         return{
-            productos: []
+            productos: [],
+            productosConPropAgregadas: [],
+            listaFiltrosChecks: [],
+
         }
     },
     created(){
@@ -20,5 +23,51 @@ createApp({
                 }
             
             })
+    },
+
+
+    methods: {
+        agregarPropiedadesFiltrosChecks() {
+            this.productosConPropAgregadas = this.productos.map( producto => {
+                let masc;
+                let precio;
+
+                
+                if(producto.descripcion.toLowerCase().includes('perr') || producto.producto.toLowerCase().includes('perr')) {
+                    masc = 'Para tu perro';
+                }
+                else if(producto.descripcion.toLowerCase().includes('gat') || producto.producto.toLowerCase().includes('gat')) {
+                    masc = 'Para tu gato';
+                }
+                else {
+                    masc = 'Para ambos';
+                }
+
+
+                if(producto.precio <= 1000 ) {
+                    precio = 'Precio Bajo';
+                }
+                else if(producto.precio > 1000 && producto.precio < 2000){
+                    precio = 'Precio Intermedio';
+                }
+                else {
+                    precio = 'Precio Alto';
+                }
+
+                
+                return {
+                    ... producto,
+                    mascota: masc,
+                    rangoPrecio: precio
+                };
+            });
+
+            let filtros = this.productosConPropAgregadas.map(prod => [ prod.mascota, prod.rangoPrecio]);
+            for(let filtro of filtros) {
+                this.listaFiltrosChecks.push(...filtro);
+            }
+            this.listaFiltrosChecks = [... new Set(this.listaFiltrosChecks)];
+        }
     }
+
 }).mount("#app")
