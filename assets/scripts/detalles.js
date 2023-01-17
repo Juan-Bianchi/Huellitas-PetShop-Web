2769807d7420089e4ee0a89b1b16ed6774e24e00
Juan-Bianchi <juanbianchi@icloud.com>
2023-01-17 17:32:10 -0300

    se unifican logicas para implementar carrito en paginas de venta

--- assets/scripts/detalles.js
+++ assets/scripts/detalles.js
@@ -7,8 +7,17 @@ createApp ( {
             productosModif: [],
             producto: undefined,
             productosCarrito: [],
-            productosPromo3x2: ["Collar de pulgas para gatos", "Hueso de goma", "Pelota dos colores para perros", "Peluche de pelota"],
             totalCompra: 0,
+            dataOrig: [],
+            productos: [],
+            productosConPropAgregadas: [],
+            listaFiltrosPrecio: [],
+            listaFiltrosMascota: [],
+            listaFiltrosPromo: [],
+            productosPorBusqueda:"",
+            productosFiltradosFinal:[],
+            checks:[],
+            productosPromo3x2: ["63a28d36cc6fff6724518aa3", "63a28d38cc6fff6724518ab3", "63a28d38cc6fff6724518abd", "63a28d39cc6fff6724518abf"],
         }
     },
     created(){
@@ -31,7 +40,22 @@ createApp ( {
                 })
                 this.productosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
                 this.actualizarStockDeLocalStorage();
+
+                if(window.location.pathname === "/index.html" ){
+                    this.productos = [...this.productosModif];
+                }else if(window.location.pathname === "/farmacia.html"){
+                    this.productos = this.productosModif.filter(producto => producto.categoria === "farmacia")
+                }else if(window.location.pathname === "/juguete.html"){
+                    this.productos = this.productosModif.filter(producto => producto.categoria === "jugueteria")
+                }
+
+                this.agregarPropiedadesFiltrosChecks();
+                this.generoListaChecks();
+                this.productosFiltradosFinal= [...this.productos]
+
                 this.producto = this.productosModif.find( prod => prod._id === id);
+
+                this.productosFiltradosFinal= [...this.productos]
                 
 
             })
@@ -124,7 +148,7 @@ createApp ( {
             prodAVender.cantPedida += acumulador;
            
 
-            if(prodAVender.cantPedida >= 3 && this.productosPromo3x2.some(prod => prod == prodAVender.producto)) {
+            if(prodAVender.cantPedida >= 3 && this.productosPromo3x2.some(prod => prod == prodAVender._id)) {
                 prodAVender.descuento = ((Math.floor(prodAVender.cantPedida / 3 )) * prodAVender.precio);
             }
             else {
@@ -144,7 +168,69 @@ createApp ( {
         limpiarLocalStorage: function() {
             localStorage.clear();
             this.productosCarrito = [];
-        }
+        },
+
+        agregarPropiedadesFiltrosChecks() {
+            this.productosConPropAgregadas = this.productos.map( producto => {
+                let masc;
+                let precio;
+                let promo;
+                
+                if(producto.descripcion.toLowerCase().includes('perr') || producto.producto.toLowerCase().includes('perr')) {
+                    masc = 'Para tu perro';
+                }
+                else if(producto.descripcion.toLowerCase().includes('gat') || producto.producto.toLowerCase().includes('gat')) {
+                    masc = 'Para tu gato';
+                }
+                else {
+                    masc = 'Para ambos';
+                }
+
+
+                if(producto.precio <= 1000 ) {
+                    precio = 'Hasta $1000';
+                }
+                else if(producto.precio > 1000 && producto.precio < 2000){
+                    precio = 'De $1000 a $2000';
+                }
+                else {
+                    precio = 'Más de $2000';
+                }
+
+                promo = this.productosPromo3x2.some(prod => prod.includes(producto._id))? 'Productos en promoción': 'No tienen promoción';
+                return {
+                    ... producto,
+                    mascota: masc,
+                    rangoPrecio: precio,
+                    promocion: promo
+                };
+            });
+        },
+
+        generoListaChecks() {
+            let listaFiltrosChecks = []
+            let filtros = this.productosConPropAgregadas.map(prod => [ prod.mascota, prod.rangoPrecio, prod.promocion]);
+            for(let filtro of filtros) {
+                listaFiltrosChecks.push(...filtro);
+            }
+            listaFiltrosChecks = [... new Set(listaFiltrosChecks)].sort();
+            this.listaFiltrosPrecio = listaFiltrosChecks.filter(categoria => categoria.startsWith('H') || categoria.startsWith('D') || categoria.startsWith('M'));
+            this.listaFiltrosMascota = listaFiltrosChecks.filter(categoria => categoria.startsWith('Pa'));
+            this.listaFiltrosPromo = listaFiltrosChecks.filter(categoria => categoria.startsWith('Pr'));
+        },
+
+        filtroCruzado: function(){
+            let filtradoPorBusqueda = this.productosConPropAgregadas.filter(elemento => elemento.producto.toLowerCase().includes( this.productosPorBusqueda.toLowerCase()));
+            if( this.checks.length === 0 ){
+                this.productosFiltradosFinal = filtradoPorBusqueda;
+                
+            }else{
+                let filtradosPorCheck = filtradoPorBusqueda.filter( producto => this.checks.includes( producto.mascota)||this.checks.includes( producto.rangoPrecio)||this.checks.includes( producto.promocion))
+                console.log(filtradosPorCheck)
+                this.productosFiltradosFinal = filtradosPorCheck; 
+            }          
+        },
+
 
     }
 
