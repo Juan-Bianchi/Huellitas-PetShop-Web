6b33b73065926f7cbb6b5dbabc39cd2a5decaa58
SLautaro <santillanlautaronahuel@gmail.com>
2023-01-17 14:26:18 -0300

    comentarios a localStorage

--- assets/scripts/contacto.js
+++ assets/scripts/contacto.js
@@ -1,50 +1,59 @@
-const{ createApp } = Vue
+const { createApp } = Vue
 
 createApp({
-    data(){
-        return{
+    data() {
+        return {
             productos: [],
-            mensaje:"",
-            mascota:"",
-            otraMascota:"",
-            telefonoUsuario:"",
-            nombreUsuario:"",
-            emailUsuario:"",
+            mensaje: "",
+            mascota: "",
+            otraMascota: "",
+            telefonoUsuario: "",
+            nombreUsuario: "",
+            emailUsuario: "",
+            comentariosUsuario: [],
         }
     },
-    created(){
+    created() {
         fetch("https://mindhub-xj03.onrender.com/api/petshop")
             .then(response => response.json())
             .then(productos => {
 
-                if(window.location.pathname === "/index.html"){
+                if (window.location.pathname === "/index.html") {
                     this.productos = productos
-                }else if(window.location.pathname === "/farmacia.html"){
+                } else if (window.location.pathname === "/farmacia.html") {
                     this.productos = productos.filter(producto => producto.categoria === "farmacia")
-                }else if(window.location.pathname === "/juguete.html"){
+                } else if (window.location.pathname === "/juguete.html") {
                     this.productos = productos.filter(producto => producto.categoria === "jugueteria")
                 }
-                
+
             })
     },
-    methods:{
+    methods: {
 
-        limpiarForm(){
-            window.location.reload()
+        enviarForm() {
+            this.localStorageComentario()
+            
         },
 
-        limpiarForm(){
-            this.mensaje=""
-            this.mascota=""
-            this.otraMascota=""
-            this.telefonoUsuario=""
-            this.nombreUsuario=""
-            this.emailUsuario=""
+        limpiarForm() {
+            this.mensaje = ""
+            this.mascota = ""
+            this.otraMascota = ""
+            this.telefonoUsuario = ""
+            this.nombreUsuario = ""
+            this.emailUsuario = ""
+        },
+
+        localStorageComentario() {
+            let comentario = { nombre: this.nombreUsuario, telefono: this.telefonoUsuario, email: this.emailUsuario, mascota: this.mascota ? this.mascota : this.otraMascota, mensaje: this.mensaje }
+            this.comentariosUsuario.push({...comentario})
+            console.log(this.comentariosUsuario)
+            localStorage.setItem('comentarios', JSON.stringify(this.comentariosUsuario))
         },
 
 
-        logmensaje (){
-            console.log(this.mensaje , this.otraMascota)
-        }   
+        logmensaje() {
+            console.log(this.mensaje, this.otraMascota)
+        }
     }
 }).mount("#appContacto")
