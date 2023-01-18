689de555f6b8bcbef0588e2225144ec65b01a260
SLautaro <santillanlautaronahuel@gmail.com>
2023-01-18 12:17:18 -0300

    ignorar faq

--- assets/scripts/faq.js
+++ assets/scripts/faq.js
@@ -8,6 +8,7 @@ createApp({
             tiempo:null,
             precios:null,
             factura:null,
+            boxplace:1,
         }
     },
     created() {
@@ -19,31 +20,40 @@ createApp({
         focusChanged(event) {
             const el = event.target
             if(el.id=='entrega') {this.entrega=1
-                this.defectos=0
+                this.defecto=0
                 this.tiempo=0
                 this.precios=0
+                this.factura=0
             }
-            if(el.id=='defecto') {this.entrega=0
+            if(el.id=='defecto') {
                 this.defecto=1
+                this.entrega=0
                 this.tiempo=0
                 this.precios=0
+                this.factura=0
             }
-            if(el.id=='tiempo') {this.entrega=0
-                this.defectos=0
+            if(el.id=='tiempo') {
                 this.tiempo=1
+                this.entrega=0
+                this.defecto=0
                 this.precios=0
+                this.factura=0
             }
-            if(el.id=='precios') {this.entrega=0
-                this.defectos=0
-                this.tiempo=0
+            if(el.id=='precios') {
                 this.precios=1
+                this.defecto=0
+                this.tiempo=0
+                this.entrega=0
+                this.factura=0
             }
-            if(el.id=='factura') {this.entrega=0
-                this.defectos=0
+            if(el.id=='factura') {
+                this.factura=1
+                this.defecto=0
                 this.tiempo=0
                 this.precios=0
-                this.factura=1
+                this.entrega=0
             }
+            this.boxplace=0
             console.log(el.id)
             
         },
