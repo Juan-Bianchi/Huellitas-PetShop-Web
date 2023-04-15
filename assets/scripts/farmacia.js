c5fb0ac9b9b939ed166491c6419a168d95bf40bf
unknown <juanbianchi@icloud.com>
2023-04-15 17:32:36 -0300

    pull desde main

---
+++ assets/scripts/farmacia.js
@@ -0,0 +1,24 @@
+const {createApp} = Vue;
+
+
+createApp({
+    data(){
+        return{
+
+        }
+    },
+
+    created(){
+
+    },
+
+    mounted(){
+
+    },
+
+    methods:{
+
+    },
+
+}).mount("#app");
+
